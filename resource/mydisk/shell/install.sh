c4df928d8631a5b536f24ed7d6b5b8b823bbcd1c
Ivan <644633699@qq.com>
2018-08-21 04:16:41 +0800

    发布

--- resource/mydisk/shell/install.sh
+++ resource/mydisk/shell/install.sh
@@ -40,6 +40,20 @@ read_var()
         done
 }
 
+read_value()
+{
+        VAR="$1"
+        
+        echo ""
+        echo -n "....${2} [Y/N]: "
+        read "$VAR"
+        
+        VALUE=$(eval echo '$'"$VAR")
+        
+        VALUE=$(echo "$VALUE" | sed "s/[ \t]//g")
+        VALUE=$(echo "$VALUE" | tr "[a-z]" "[A-Z]")
+}
+
 #### Operation
 mkdir -p ${UDISK_ROOT}/mydisk/tmp
 
@@ -47,11 +61,13 @@ mkdir -p ${UDISK_ROOT}/mydisk/tmp
 echo "正在配置 CentOS:"
 echo ""
 
-read_var "IPADDR"         "....请输入你的IP" "192.168.1.5"     "$IP_FORMAT"                   "break"      "echo_error"
-read_var "GATEWAY"        "..请输入你的网关" "192.168.1.1"     "$IP_FORMAT"                   "break"      "echo_error"
-read_var "DNS"            "...请输入你的DNS" "114.114.114.114" "$IP_FORMAT"                   "break"      "echo_error"
-read_var "HOSTNAME"       "请输入你的主机名" "mydisk"          "^[A-Za-z_][A-Za-z0-9_\-\.]*$" "break"      "echo_error"
-read_var "ROOT_PASSWORD"  "..请输入root密码" "123456"          "'"                            "echo_error" "break"
-read_var "ADMIN_PASSWORD" ".请输入admin密码" "123456"          "'"                            "echo_error" "break"
+read_var   "IPADDR"         "....请输入你的IP" "192.168.1.5"     "$IP_FORMAT"                   "break"      "echo_error"
+read_var   "GATEWAY"        "..请输入你的网关" "192.168.1.1"     "$IP_FORMAT"                   "break"      "echo_error"
+read_var   "DNS"            "...请输入你的DNS" "114.114.114.114" "$IP_FORMAT"                   "break"      "echo_error"
+read_var   "HOSTNAME"       "请输入你的主机名" "mydisk"          "^[A-Za-z_][A-Za-z0-9_\-\.]*$" "break"      "echo_error"
+read_var   "ROOT_PASSWORD"  "..请输入root密码" "123456"          "'"                            "echo_error" "break"
+read_var   "ADMIN_PASSWORD" ".请输入admin密码" "123456"          "'"                            "echo_error" "break"
+read_value "MOUNT_UDISK"    ".是否自动挂载U盘"
+read_value "MOUNT_DISK"     "..是否挂载新硬盘"
 
 #### End
