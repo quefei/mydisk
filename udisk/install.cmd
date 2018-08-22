04e223cd38baac52521b3156f3f41f95e1b957aa
Ivan <644633699@qq.com>
2018-08-23 00:25:36 +0800

    发布

--- udisk/install.cmd
+++ udisk/install.cmd
@@ -4,13 +4,13 @@ title %~nx0
 set UdiskLabel=MYDISK
 set KeyName=HKLM\SYSTEM\CurrentControlSet\Services\USBSTOR\Enum
 set Git=Git-2.18.0-64-bit.exe
-set GitSrc=%~d0\mydisk\resource\%Git%
+set GitSrc=%~d0\install\resource\%Git%
 set GitBash="C:\Program Files\Git\git-bash.exe"
 
 REM U盘卷标
 for /f "tokens=1-3" %%a in ('wmic logicaldisk get Description^,DeviceID^,VolumeName 2^>nul') do (
         if /i "%%a"=="可移动磁盘" (
-                if /i "%%b\mydisk\cmd\"=="%~dp0" (
+                if /i "%%b\"=="%~dp0" (
                         set DeviceID=%%b
                 )
         )
@@ -22,17 +22,6 @@ if defined DeviceID (
         call :EchoError "请将此程序复制到U盘根目录下运行"
 )
 
-REM 复制uninstall.cmd
-if not exist %~d0\LMT (
-        mkdir %~d0\LMT
-)
-
-if exist %~dp0uninstall.cmd (
-        copy /y %~dp0uninstall.cmd %~d0\LMT>nul
-) else (
-        call :EchoError "%~dp0uninstall.cmd 文件不存在"
-)
-
 REM U盘属性值
 echo,如果需要自动挂载额外的U盘, 请插入U盘!
 echo,
@@ -40,11 +29,11 @@ echo,
 echo,请按任意键继续...
 pause>nul
 
-if not exist %~d0\mydisk\tmp (
-        mkdir %~d0\mydisk\tmp
+if not exist %~d0\install\tmp (
+        mkdir %~d0\install\tmp
 )
 
-type nul>%~d0\mydisk\tmp\udisk.tmp
+type nul>%~d0\install\tmp\udisk.tmp
 
 for /f "tokens=1-3" %%a in ('reg query "%KeyName%" /v Count 2^>nul') do (
         if /i "%%a"=="Count" (
@@ -64,7 +53,7 @@ for /l %%a in (1,1,%CountVar%) do (
                         set PID=!Attr:~17,4!
                         set SN=!Attr:~22!
                         
-                        echo,VID: !VID! PID: !PID! SN: !SN!>>%~d0\mydisk\tmp\udisk.tmp
+                        echo,VID: !VID! PID: !PID! SN: !SN!>>%~d0\install\tmp\udisk.tmp
                 )
         )
 )
@@ -89,11 +78,13 @@ if not exist %GitBash% (
 )
 
 REM 运行install.sh
-if exist %~d0\mydisk\shell\install.sh (
-        start %GitBash% %~d0\mydisk\shell\install.sh
+if exist %~d0\install\shell\install.sh (
+        start %GitBash% %~d0\install\shell\install.sh
 ) else (
-        call :EchoError "%~d0\mydisk\shell\install.sh 文件不存在"
+        call :EchoError "%~d0\install\shell\install.sh 文件不存在"
 )
+
+del /f %~0
 
 exit
 
