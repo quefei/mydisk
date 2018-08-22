dc9ae522cb355c48d938a185a5725e9113a4fe55
Ivan <644633699@qq.com>
2018-08-22 13:25:55 +0800

    发布

--- udisk/mydisk/shell/install.sh
+++ udisk/mydisk/shell/install.sh
@@ -2,6 +2,17 @@
 
 set -eu
 
+SCRIPT_LIST="var.sh func.sh"
+
+for SCRIPT in ${SCRIPT_LIST}; do
+        if [[ -s "$SCRIPT" ]]; then
+                . "$SCRIPT"
+        else
+                read -n1 -p "Error: ${SCRIPT} not found"
+                exit 1
+        fi
+done
+
 #### Variable
 
 #### Function
