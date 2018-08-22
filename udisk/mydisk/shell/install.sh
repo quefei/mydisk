a97a4f862486e8ec2c5cfbfd6a0bbb8c9532fbdb
Ivan <644633699@qq.com>
2018-08-22 11:25:54 +0800

    发布

--- udisk/mydisk/shell/install.sh
+++ udisk/mydisk/shell/install.sh
@@ -3,9 +3,18 @@
 set -eu
 
 #### Variable
+SH_LIST="var.sh func.sh"
 
 #### Function
 
 #### Operation
+for SH in ${SH_LIST}; do
+        if [[ -s "./${SH}" ]]; then
+                . ./${SH}
+        else
+                read -n1 -p "Error: ${SH} not found"
+                exit
+        fi
+done
 
 #### End
