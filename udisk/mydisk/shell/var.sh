a92a07b40d111adfd893f2304ac3e036fbdc29ae
Ivan <644633699@qq.com>
2018-08-22 18:36:09 +0800

    发布

--- udisk/mydisk/shell/var.sh
+++ udisk/mydisk/shell/var.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
 
 VERSION="1804"
+
 CENTOS="CentOS-7-x86_64-Minimal-${VERSION}.iso"
 CENTOS_MD5="fabdc67ff3a1674a489953effa285dfd"
 CENTOS_SRC="${ROOTDIR}/mydisk/resource/${CENTOS}"
 CENTOS_DEST="${ROOTDIR}/${CENTOS}"
+
 MYDISK="Mydisk-${VERSION}.iso"
 MYDISK_MD5="810f146cb8119457c34b04f64d490894"
 MYDISK_SRC="${ROOTDIR}/mydisk/resource/${MYDISK}"
 MYDISK_DEST="${ROOTDIR}/LMT/${MYDISK}"
 MYDISK_URL="https://github.com/quefei/mydisk/raw/master/ol/lfs/${MYDISK}"
+
+KS_SRC="${ROOTDIR}/mydisk/resource/ks.cfg.default"
+KS_DEST="${ROOTDIR}/ks.cfg"
+
 DOWNLOAD_DEST="${ROOTDIR}/mydisk/tmp/download.tmp"
 DOWNLOAD_URL="https://gitee.com/quefei/mydisk/raw/master/ol/resource/${CENTOS}.download"
 
 NULL="${ROOTDIR}/mydisk/tmp/null.tmp"
-KS_SRC="${ROOTDIR}/mydisk/resource/ks.cfg.default"
-KS_DEST="${ROOTDIR}/ks.cfg"
-UDISK_TMP="${ROOTDIR}/mydisk/tmp/udisk.tmp"
-DISK_TMP="${ROOTDIR}/mydisk/tmp/disk.tmp"
+DISK="${ROOTDIR}/mydisk/tmp/disk.tmp"
+UDISK="${ROOTDIR}/mydisk/tmp/udisk.tmp"
 
 MAX="100"
-REBOOT="reboot"
+COMPLETE="reboot"
 
 IP_FORMAT="^[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*$"
 HN_FORMAT="^[A-Za-z_][A-Za-z0-9_\-\.]*$"
