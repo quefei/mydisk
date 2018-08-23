29818d50bdb0f15aea2cd533b27675565c5f5314
Ivan <644633699@qq.com>
2018-08-23 13:57:09 +0800

    发布

--- udisk/mydisk/shell/install.sh
+++ udisk/mydisk/shell/install.sh
@@ -23,12 +23,12 @@ mkdir -p ${ROOTDIR}/mydisk/tmp
 ## display 1
 echo_head "正在配置 CentOS:"
 
-config_centos "....请输入你的IP" "$DEFAULT_IPADDR"   "$IP_REGEXP"       "break" "echo_error" && IPADDR="$READ_VAR"
-config_centos "..请输入你的网关" "$DEFAULT_GATEWAY"  "$IP_REGEXP"       "break" "echo_error" && GATEWAY="$READ_VAR"
-config_centos "...请输入你的DNS" "$DEFAULT_DNS"      "$IP_REGEXP"       "break" "echo_error" && DNS="$READ_VAR"
-config_centos "请输入你的主机名" "$DEFAULT_HOSTNAME" "$HOSTNAME_REGEXP" "break" "echo_error" && HOSTNAME="$READ_VAR"
-config_centos "..请输入root密码" "$DEFAULT_PASSWORD" "$PASSWORD_REGEXP" "echo_error" "break" && ROOT_PASSWORD="$READ_VAR"
-config_centos ".请输入admin密码" "$DEFAULT_PASSWORD" "$PASSWORD_REGEXP" "echo_error" "break" && ADMIN_PASSWORD="$READ_VAR"
+config_centos "....请输入你的IP" "$DEFAULT_IPADDR"   "$IP_REGEXP"       "break"      "echo_error" && IPADDR="$READ_VAR"
+config_centos "..请输入你的网关" "$DEFAULT_GATEWAY"  "$IP_REGEXP"       "break"      "echo_error" && GATEWAY="$READ_VAR"
+config_centos "...请输入你的DNS" "$DEFAULT_DNS"      "$IP_REGEXP"       "break"      "echo_error" && DNS="$READ_VAR"
+config_centos "请输入你的主机名" "$DEFAULT_HOSTNAME" "$HOSTNAME_REGEXP" "break"      "echo_error" && HOSTNAME="$READ_VAR"
+config_centos "..请输入root密码" "$DEFAULT_PASSWORD" "$PASSWORD_REGEXP" "echo_error" "break"      && ROOT_PASSWORD="$READ_VAR"
+config_centos ".请输入admin密码" "$DEFAULT_PASSWORD" "$PASSWORD_REGEXP" "echo_error" "break"      && ADMIN_PASSWORD="$READ_VAR"
 
 
 
