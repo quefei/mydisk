f30cc83153c885c681a97c82d90ca8d49350863c
Ivan <644633699@qq.com>
2018-08-23 12:05:25 +0800

    发布

--- udisk/mydisk/shell/function.sh
+++ udisk/mydisk/shell/function.sh
@@ -1,1 +1,57 @@
 #!/bin/bash
+
+echo_head()
+{
+        clear
+        echo "$1"
+        echo ""
+}
+
+echo_error()
+{
+        echo ""
+        echo "Error: 输入错误, 请重新输入!"
+}
+
+read_tail()
+{
+        echo ""
+        echo ""
+        read -n1 -p "请按任意键${1}... "
+}
+
+read_error()
+{
+        read -n1 -p "Error: ${1}! "
+        exit 1
+}
+
+check_file()
+{
+        if [[ ! -s "$1" ]]; then
+                read_error "${1} 文件不存在"
+        fi
+        
+        sed -i "s/\r$//g" ${1}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
