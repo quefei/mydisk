f255369cfbe92a76074deb70d7ab636ebcbef86a
Ivan <644633699@qq.com>
2018-08-24 14:00:55 +0800

    发布

---
+++ mydisk/shell/script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
+#ARG1
+
+set -ueo pipefail
+#set -x
+
+#### Variable
+
+#### Function
+
+#### Operation
+
+#### End
