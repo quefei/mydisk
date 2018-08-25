571edd9b414e5eee4e74f4f855f0dbb87530671a
Ivan <644633699@qq.com>
2018-08-25 11:16:44 +0800

    发布

--- mydisk/shell/install.sh
+++ mydisk/shell/install.sh
@@ -10,9 +10,9 @@ set -ueo pipefail
 . ${ROOTDIR}/mydisk/shell/install_fn.sh
 
 #### Operation
+## display 1
 mkdir -p ${ROOTDIR}/mydisk/tmp
 
-## display 1
 echo_head "正在配置 CentOS:"
 
 config_centos "....请输入你的IP" "$DEFAULT_IPADDR"   "$IP_REGEXP"       "break"      "echo_error" && IPADDR="$READ_VAR"
