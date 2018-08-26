a57d20c139178780f770cd8b0db479ed6ca9a6eb
Ivan <644633699@qq.com>
2018-08-26 17:21:34 +0800

    发布

--- mydisk/shell/install_fn.sh
+++ mydisk/shell/install_fn.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 set -ueo pipefail
-#set -x
 
-# echo_head "arg1"
 echo_head()
 {
         clear
@@ -11,14 +9,12 @@ echo_head()
         echo ""
 }
 
-# echo_error
 echo_error()
 {
         echo ""
         echo "Error: 输入错误, 请重新输入!"
 }
 
-# read_tail "arg1"
 read_tail()
 {
         echo ""
@@ -26,7 +22,6 @@ read_tail()
         read -n1 -p "请按任意键${1}... "
 }
 
-# read_command "arg1"        $READ_VAR
 read_command()
 {
         echo ""
@@ -36,7 +31,6 @@ read_command()
         READ_VAR=$(echo "$READ_VAR" | sed "s/[ \t]//g")
 }
 
-# config_centos "arg1" "arg2" "arg3" "arg4" "arg5"        $READ_VAR
 config_centos()
 {
         for NUM in $(seq ${READ_MAX}); do
@@ -54,7 +48,6 @@ config_centos()
         done
 }
 
-# use_mount "arg1"        $READ_VAR
 use_mount()
 {
         read_command "${1} [Y/N]"
@@ -66,7 +59,6 @@ use_mount()
         fi
 }
 
-# config_disk "arg1" "arg2"        $READ_VAR
 config_disk()
 {
         read_command "${1} ${NUMBER} (例如:${2}${NUM})"
